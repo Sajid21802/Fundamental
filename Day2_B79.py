1f23f304b503a30eca83f707cd05fedd2928ea84
Sajid21802 <94834018+Sajid21802@users.noreply.github.com>
2023-07-13 09:34:19 +0530

    Add files via upload

---
+++ Day2_B79.py
@@ -0,0 +1,100 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import this
+
+
+# In[2]:
+
+
+x = 9
+y = 8
+
+
+# In[4]:
+
+
+z= x+y
+print (z)
+
+
+# In[5]:
+
+
+First name = Sajid
+
+
+# In[ ]:
+
+
+firstname = 'Sajid'
+
+
+# In[1]:
+
+
+print(firstname)
+
+
+# In[3]:
+
+
+1name = 'sajid'
+
+print(name1)
+
+
+# In[4]:
+
+
+name = 'sajid' #single quote   
+
+print(name)
+
+
+# In[5]:
+
+
+firstname = 'sajid'
+
+print(firstname)
+
+
+# In[6]:
+
+
+name@ = 'sajid'
+
+print(name@)
+
+
+# In[7]:
+
+
+name2 = "sajid"       #double quote
+
+print(name2)
+
+
+# In[8]:
+
+
+fullname = 'sajid khan'
+
+print(fullname)
+
+
+# In[9]:
+
+
+print(fullname.title())
+
+
+# In[ ]:
+
+
+
+
