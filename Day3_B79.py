81bd41d2a90607672cf58bb1dbef8747af66f063
Sajid21802 <94834018+Sajid21802@users.noreply.github.com>
2023-07-14 09:32:12 +0530

    Add files via upload

---
+++ Day3_B79.py
@@ -0,0 +1,129 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+firstname = 'sajid'
+
+
+# In[2]:
+
+
+lastname = 'khan'
+
+
+# In[5]:
+
+
+# i want the fullname......?
+
+
+# In[ ]:
+
+
+f strings :
+    
+# Genral syntax of f strings :
+    
+f"custom message , {place_holder-1} {place_holder 2}...... {place_holder_n}''
+
+
+# In[22]:
+
+
+fullname = f'{firstname} {lastname}'
+
+print(fullname.title())
+
+
+# In[23]:
+
+
+message =f'keep up the good work, {fullname}'
+
+print(message)
+
+
+# In[1]:
+
+
+fav_lang = "python,javasript,c,c++"
+
+print(fav_lang)
+
+
+# In[2]:
+
+
+print('fav_lang: \npython\njavascript\nc\nc++')
+
+
+# In[3]:
+
+
+print('fav_lang: \n\tpython\n\tjavascript\n\tc\n\tc++')
+
+
+# In[4]:
+
+
+language = 'python      '
+
+
+print(language)
+
+
+# In[7]:
+
+
+language2 = '    python'
+
+
+print(language2)
+
+
+# In[8]:
+
+
+language3 = '    python      '
+
+
+print(language3)
+
+
+# In[13]:
+
+
+language.rstrip()
+
+
+# In[12]:
+
+
+language2.lstrip()
+
+
+# In[14]:
+
+
+language3.strip()
+
+
+# In[15]:
+
+
+language.strip()
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
